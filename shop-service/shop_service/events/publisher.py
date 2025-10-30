c654b0213e3ac91f937f1ce8485136ff7f0bad2e
Ebil Abilli <ebilebilli3gmail.com>
2025-10-30 11:14:35 +0400

    event tested and some problems fixed.event now successfuly sends  to rabitmq

--- shop-service/shop_service/events/publisher.py
+++ shop-service/shop_service/events/publisher.py
@@ -3,21 +3,21 @@ import pika
 import os
 
 
-RABBITMQ_HOST = os.getenv("RABBITMQ_HOST", "rabbitmq")
-RABBITMQ_USER = os.getenv("RABBITMQ_USER", "admin")
-RABBITMQ_PASS = os.getenv("RABBITMQ_PASS", "supersecret123")
+RABBITMQ_HOST = os.getenv('RABBITMQ_HOST', 'rabbitmq')
+RABBITMQ_USER = os.getenv('RABBITMQ_USER', 'admin')
+RABBITMQ_PASS = os.getenv('RABBITMQ_PASS', 'supersecret123')
 
 
 def publish_event(event_name: str, payload: dict):
     credentials = pika.PlainCredentials(RABBITMQ_USER, RABBITMQ_PASS)
     connection = pika.BlockingConnection(pika.ConnectionParameters(RABBITMQ_HOST, credentials=credentials))
     channel = connection.channel()
-    channel.queue_declare(queue="user_events")
+    channel.queue_declare(queue='user_events')
 
     message = {
-        "event": event_name,
-        "data": payload,
+        'event': event_name,
+        'data': payload,
     }
 
-    channel.basic_publish(exchange="", routing_key="user_events", body=json.dumps(message))
+    channel.basic_publish(exchange='', routing_key='user_events', body=json.dumps(message, default=str))
     connection.close()
